b4d8d07c357c9b739645028d94e8e377528c9669
Zhen Chen <chenzhen@rock-chips.com>
2023-01-13 10:54:33 +0800

    F: rk_gralloc_select_format(): not to use AFBC for HAL_PIXEL_FORMAT_DEPTH_32F
    用于修复 "https://redmine.rock-chips.com/issues/392352" 中分配 graphic_buffer 失败的异常.
    
    Change-Id: I6119c16fdfc4bece8b852bd88a8a9d78ea073435

--- src/core/formats.cpp
+++ src/core/formats.cpp
@@ -1673,6 +1673,7 @@ static internal_format_t rk_gralloc_select_format(const mali_gralloc_android_for
                                                 && internal_format != MALI_GRALLOC_FORMAT_INTERNAL_RGBA_16161616
                                                 && internal_format != MALI_GRALLOC_FORMAT_INTERNAL_NV16
                                                 && internal_format != MALI_GRALLOC_FORMAT_INTERNAL_BGR_888
+                                                && internal_format != HAL_PIXEL_FORMAT_DEPTH_32F
                                                 && should_sf_client_layer_use_afbc_format_by_size(internal_format,
                                                                                                   buffer_size) )
                                         {
